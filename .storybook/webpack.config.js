d0ad450eabd474ca8d6917188ca10059ab786697
Alexander Paldy <alexander.paldy@vacuumlabs.com>
2020-01-13 09:26:42 +0100

    [PM-887] Fix storybook to load antd theme

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,40 @@
+const AntdScssThemePlugin = require('antd-scss-theme-plugin')
+
 module.exports = ({config}) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('awesome-typescript-loader'),
-      },
-      // Optional
-      {
-        loader: require.resolve('react-docgen-typescript-loader'),
-      },
-    ],
-  })
-  config.resolve.extensions.push('.ts', '.tsx')
+  config.plugins.push(new AntdScssThemePlugin('./src/theme.scss'))
+
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('awesome-typescript-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    },
+    {
+      test: /\.less$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+        },
+        AntdScssThemePlugin.themify({
+          loader: 'less-loader',
+          options: {
+            javascriptEnabled: true,
+          },
+        }),
+      ],
+    },
+  )
+  config.resolve.extensions.push('.ts', '.tsx', '.less')
+
   return config
 }
