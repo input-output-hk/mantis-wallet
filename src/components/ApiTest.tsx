9623b1fdcf518c14b71873449ea33d4bf31291d3
Mice Pápai <qwhexz@gmail.com>
2020-01-31 16:44:37 +0100

    [PM-1304] add ApiTest fields

--- src/components/ApiTest.tsx
+++ src/components/ApiTest.tsx
@@ -1,28 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {web3, SpendingKey, SeedPhrase, TransparentAddress, Account} from '../web3'
 import {WalletState} from '../common/wallet-state'
+import {DialogInput} from '../common/dialog/DialogInput'
 
 const wallet = web3.midnight.wallet
 
 // FIXME: remove this component after every needed method is wired up with the real interface
 export const ApiTest = (): JSX.Element => {
   const state = WalletState.useContainer()
-  const passphrase = 'Foobar1234'
-  const spendingKey = 'm-test-shl-sk1fj335eanpmupaj9vx5879t7ljfnh7xct486rqgwxw8evwp2qkaksmcqu88'
-  const seedPhrase = [
-    'pencil',
-    'ensure',
-    'regular',
-    'job',
-    'joy',
-    'witness',
-    'dutch',
-    'rain',
-    'minimum',
-    'wealth',
-    'estate',
-    'leopard',
-  ]
+
+  const [passphrase, setPassphrase] = useState<string>('Foobar1234')
+  const [spendingKey, setSpendingKey] = useState<string>(
+    'm-test-shl-sk1fj335eanpmupaj9vx5879t7ljfnh7xct486rqgwxw8evwp2qkaksmcqu88',
+  )
+  const [seedPhrase, setSeedPhrase] = useState<string>(
+    'pencil ensure regular job joy witness dutch rain minimum wealth estate leopard',
+  )
 
   const call = (fn: CallableFunction) => (): void => {
     if (state.walletStatus === 'LOADED' || state.walletStatus === 'ERROR') state.reset()
@@ -48,7 +41,22 @@ export const ApiTest = (): JSX.Element => {
       </div>
 
       <div>
-        {/* You can call any wallet function as usual, but the calls will be async */}
+        <DialogInput
+          label="Passphrase"
+          value={passphrase}
+          onChange={(e): void => setPassphrase(e.target.value)}
+        />
+        <DialogInput
+          label="Spending Key"
+          value={spendingKey}
+          onChange={(e): void => setSpendingKey(e.target.value)}
+        />
+        <DialogInput
+          label="Seed Phrase"
+          value={seedPhrase}
+          onChange={(e): void => setSeedPhrase(e.target.value)}
+        />
+
         <TestButton onClick={(): Promise<SpendingKey & SeedPhrase> => wallet.create({passphrase})}>
           Create
         </TestButton>
@@ -70,7 +78,9 @@ export const ApiTest = (): JSX.Element => {
           Restore from Spending Key
         </TestButton>
         <TestButton
-          onClick={(): Promise<boolean> => wallet.restoreFromSeedPhrase({passphrase, seedPhrase})}
+          onClick={(): Promise<boolean> =>
+            wallet.restoreFromSeedPhrase({passphrase, seedPhrase: seedPhrase.split(' ')})
+          }
         >
           Restore from Seed Phrase
         </TestButton>
