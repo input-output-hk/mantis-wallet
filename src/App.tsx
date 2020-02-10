d937ae50c2f0fc4e7107287075f87c4dde3a8ae8
Alexander Paldy <alexander.paldy@vacuumlabs.com>
2020-02-10 13:37:02 +0100

    [PM-1358] Quick UI fixes
     - Change 'Overview Wallet' to 'Wallet Overview'
     - Make polling less frequent when backend is online
     - Bring sync status popup over every element

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ const App: React.FC = () => {
       try {
         await Promise.all([web3.midnight.wallet.listAccounts(), web3.version.ethereum])
         setBackendRunning(true)
+        setTimeout(checkBackend, 5000)
       } catch (e) {
         setBackendRunning(false)
-      } finally {
         setTimeout(checkBackend, 1000)
       }
     }
