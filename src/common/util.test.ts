aa386bffa22fb028d091bb562c2e9c89bd1041ac
Alexander Paldy <alexander.paldy@vacuumlabs.com>
2020-02-12 10:14:09 +0100

    Fix tests - actual and expected values were exchanged previously

--- src/common/util.test.ts
+++ src/common/util.test.ts
@@ -3,12 +3,12 @@ import Big from 'big.js'
 import {deserializeBigNumber, bigToNumber} from './util'
 
 it('deserializes BigNumber correctly', () => {
-  assert.deepEqual(Big(2), deserializeBigNumber({s: 1, e: 0, c: [2]}))
-  assert.deepEqual(Big(-2), deserializeBigNumber({s: -1, e: 0, c: [2]}))
-  assert.deepEqual(Big(2.3), deserializeBigNumber({s: 1, e: 0, c: [2, 3]}))
-  assert.deepEqual(Big(23.4), deserializeBigNumber({s: 1, e: 1, c: [2, 3, 4]}))
+  assert.deepEqual(deserializeBigNumber({s: 1, e: 0, c: [2]}), Big(2))
+  assert.deepEqual(deserializeBigNumber({s: -1, e: 0, c: [2]}), Big(-2))
+  assert.deepEqual(deserializeBigNumber({s: 1, e: 0, c: [2, 3]}), Big(2.3))
+  assert.deepEqual(deserializeBigNumber({s: 1, e: 1, c: [2, 3, 4]}), Big(23.4))
 })
 
 it('converts Big to number correctly', () => {
-  assert.equal(1.2345, bigToNumber(Big(1.2345)))
+  assert.equal(bigToNumber(Big(1.2345)), 1.2345)
 })
