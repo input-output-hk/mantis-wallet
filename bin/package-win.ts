038b9bce857fac0cc459c34d7df490f1da0c9828
Andrzej Kopeć <andrzej.kopec@iohk.io>
2020-03-26 11:10:35 +0100

    [Chore] Add windows readme to automatic packaging (#104)
    * [Chore] Add windows readme to automatic packaging
    
    * [Chore] Update fs usage to promisified version

--- bin/package-win.ts
+++ bin/package-win.ts
@@ -1,7 +1,6 @@
 import {packageLuna} from './lib/package'
 import * as path from 'path'
-import {promisify} from 'util'
-import * as fs from 'fs'
+import {promises as fs} from 'fs'
 
 packageLuna({
   platform: 'win32',
@@ -12,10 +11,14 @@ packageLuna({
       console.log(`Writing Windows-specific configuration to ${targetPlatformConfigPath} ...`)
       const platformConfig = {runClients: false}
 
-      return promisify(fs.writeFile)(
-        targetPlatformConfigPath,
-        JSON.stringify(platformConfig),
-      ).then(() => cb())
+      return fs.writeFile(targetPlatformConfigPath, JSON.stringify(platformConfig)).then(() => cb())
+    },
+    (buildPath, electronVersion, platform, arch, cb) => {
+      const currentReadmePath = path.resolve(__dirname, 'resources', 'README-WIN.txt')
+      const targetPlatformReadmePath = path.resolve(buildPath, '..', '..', 'README.txt')
+      console.log(`Writing Windows-specific README to ${targetPlatformReadmePath} ...`)
+
+      return fs.copyFile(currentReadmePath, targetPlatformReadmePath).then(() => cb())
     },
   ],
 })
