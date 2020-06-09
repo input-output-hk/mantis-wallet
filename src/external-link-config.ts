c5000d5e3952a3d72ead02fc5c554cbe496ae7a7
Andrzej Kopeć <andrzej.kopec@iohk.io>
2020-06-10 00:02:04 +0200

    Release/0.13.0 (#186)

--- src/external-link-config.ts
+++ src/external-link-config.ts
@@ -1,9 +1,10 @@
 export const LINKS = {
-  aboutPoB: 'https://www.testnet-pupa.project42.iohkdev.io/en/get-started/proof-of-burn/',
-  aboutGlacier: 'https://www.testnet-pupa.project42.iohkdev.io/en/get-started/glacier-drop/',
-  proverListMetrics: 'https://www.testnet-pupa.project42.iohkdev.io//en/get-started/proof-of-burn/',
-  faucet: 'https://www.testnet-pupa.project42.iohkdev.io/en/tools/faucet/',
-  explorer: 'https://explorer.testnet-pupa.project42.iohkdev.io',
+  aboutPoB: 'https://www.testnet-sonic.project42.iohkdev.io/en/get-started/proof-of-burn/',
+  aboutGlacier: 'https://www.testnet-sonic.project42.iohkdev.io/en/get-started/glacier-drop/',
+  proverListMetrics:
+    'https://www.testnet-sonic.project42.iohkdev.io//en/get-started/proof-of-burn/',
+  faucet: 'https://www.testnet-sonic.project42.iohkdev.io/en/tools/faucet/',
+  explorer: 'https://explorer.testnet-sonic.project42.iohkdev.io',
   support: 'https://input-output-rnd.slack.com/archives/C013F4GFSTX',
   feedback:
     'https://docs.google.com/forms/d/e/1FAIpQLScpNh7eur-NnWeK9gGKIXnNQ0v55zQowj26i9-fTjnPH7GX_g/formResponse',
