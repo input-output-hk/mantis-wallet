08a4c0c51f184fbba30b31ff52c9bdd2ae02f5c3
Alexander Paldy <alexander.paldy@vacuumlabs.com>
2020-07-06 08:55:50 +0200

    fixup stringify plain objects

--- src/main/logger.ts
+++ src/main/logger.ts
@@ -1,5 +1,18 @@
 import path from 'path'
+import _ from 'lodash/fp'
 import {config} from '../config/main'
 import {createLogger} from '../shared/utils'
 
 export const mainLog = createLogger('main', () => path.join(config.dataDir, 'logs', 'main.log'))
+
+// eslint-disable-next-line fp/no-mutating-methods
+mainLog.hooks.push((message, transport) => {
+  if (transport !== mainLog.transports.console) {
+    return message
+  }
+
+  return {
+    ...message,
+    data: message.data.map((v) => (_.isPlainObject(v) ? JSON.stringify(v, null, 2) : v)),
+  }
+})
