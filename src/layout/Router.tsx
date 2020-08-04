64e86375f6e9cf0f7b2fd46884303ca3e39f352f
Alexander Paldy <alexander.paldy@vacuumlabs.com>
2020-08-04 09:22:28 +0200

    [Refactor] Reorganize App component
    Move global styles to index.scss, move header element to Sidebar
    and main element to Router (with its styles).

--- src/layout/Router.tsx
+++ src/layout/Router.tsx
@@ -1,13 +1,18 @@
 import React, {useEffect} from 'react'
 import {RouterState} from '../router-state'
 import {RouteId} from '../routes-config'
+import './Router.scss'
 
 export const Router = (): JSX.Element => {
   const {
     currentRoute: {component: Component},
   } = RouterState.useContainer()
 
-  return <Component />
+  return (
+    <main className="Router">
+      <Component />
+    </main>
+  )
 }
 
 interface NavigateProps {
