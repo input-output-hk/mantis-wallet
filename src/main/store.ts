bd3907ce5321004d459ced7f9e43ec72daa986ca
Mice Pápai <qwhexz@gmail.com>
2020-11-17 14:53:24 +0100

    [ETCM-308] refactor main/createStore

--- src/main/store.ts
+++ src/main/store.ts
@@ -1,5 +1,4 @@
 import ElectronStore from 'electron-store'
-import {config} from '../config/main' // FIXME
 import {NetworkName} from '../config/type'
 import {Language, DEFAULT_LANGUAGE} from '../shared/i18n'
 import {CheckedDatadir} from './data-dir'
@@ -9,16 +8,20 @@ type StoreData = {
   'networkName': NetworkName
 }
 
-const DEFAULT_DATA: StoreData = {
-  'settings.language': DEFAULT_LANGUAGE,
-  'networkName': config.networkName,
-}
-
 export type MainStore = ElectronStore<StoreData>
 
-export const createStore = (checkedDatadir: CheckedDatadir): MainStore =>
-  new ElectronStore<StoreData>({
+export const createStore = (
+  checkedDatadir: CheckedDatadir,
+  networkName: NetworkName,
+): MainStore => {
+  const DEFAULT_DATA: StoreData = {
+    'settings.language': DEFAULT_LANGUAGE,
+    'networkName': networkName,
+  }
+
+  return new ElectronStore<StoreData>({
     cwd: checkedDatadir.datadirPath,
     defaults: DEFAULT_DATA,
     watch: true,
   })
+}
