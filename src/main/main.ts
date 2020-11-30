b7bd2b81073361bbca80aca93d76c041e1469b42
Mice Pápai <qwhexz@gmail.com>
2020-11-30 16:05:52 +0100

    [FIX] Support 1366x768

--- src/main/main.ts
+++ src/main/main.ts
@@ -61,8 +61,9 @@ function createWindow(t: TFunctionMain): void {
     icon: IS_LINUX ? LINUX_ICON : undefined,
     width,
     height,
-    minWidth: 1440,
-    minHeight: 950,
+    // Minimum supported resolution: 1366x768
+    minWidth: 1120,
+    minHeight: 680,
     webPreferences: {
       nodeIntegration: true,
     },
